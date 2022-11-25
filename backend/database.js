f12b8fcf52541193e0f133392973a32da5a43634
Kamlesh Pokiya <kamleshpokiya1993@gmail.com>
2022-11-25 19:09:25 +0530

    make it better

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://mongo:d576F3YLwzRLWEkN@cluster0.ywhjhfn.mongodb.net/personal?retryWrites=true&w=majority";
+
+const connection = `mongodb+srv://
+    ${process.env.DB_USER_NAME}:
+    ${process.env.DB_PASSWORD}@cluster0.ywhjhfn.mongodb.net/
+    ${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
+
 mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
