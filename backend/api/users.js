e827fa52c18e7e6675d646dff118f6dbf83371de
Kamlesh Pokiya <kamleshpokiya1993@gmail.com>
2022-11-25 18:39:23 +0530

    make it better

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Get all users
 router.get('/', (req, res) => {
